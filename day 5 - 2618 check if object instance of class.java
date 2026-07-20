dddccb7e5b5967e9bca961327fb42af9e87efb9a
hariharan54547-debug <hariharan54547@gmail.com>
2026-07-21 00:05:32 +0530

    Create day 5 - 2618 check if object instance of class.java

---
+++ day 5 - 2618 check if object instance of class.java
@@ -0,0 +1,23 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (obj === null || obj === undefined || typeof classFunction !== 'function') {
+        return false;
+    }
+
+    // Box primitives so we can walk their prototype chain
+    obj = Object(obj);
+
+    let proto = Object.getPrototypeOf(obj);
+    while (proto !== null) {
+        if (proto === classFunction.prototype) {
+            return true;
+        }
+        proto = Object.getPrototypeOf(proto);
+    }
+
+    return false;
+};
