4d98572d4434c6521e7ffbf5ba597ac81f80ce26
hariharan54547-debug <hariharan54547@gmail.com>
2026-07-22 09:20:18 +0530

    Create day 7 - 1768 merge strings alternately.java

---
+++ day 7 - 1768 merge strings alternately.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0;
+        int n1 = word1.length(), n2 = word2.length();
+        
+        while (i < n1 || j < n2) {
+            if (i < n1) {
+                sb.append(word1.charAt(i));
+                i++;
+            }
+            if (j < n2) {
+                sb.append(word2.charAt(j));
+                j++;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
